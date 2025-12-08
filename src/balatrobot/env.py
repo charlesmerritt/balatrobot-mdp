a79c42a61bfe3485995d56223dc6b1e6e28ed307
Milo Bauman <mjb01161@uga.edu>
2025-12-07 22:26:40 -0500

    Stronger penalty for invalid plays

--- src/balatrobot/env.py
+++ src/balatrobot/env.py
@@ -208,10 +208,10 @@ class BalatroEnv(gym.Env):
         except BalatroError as e:
             # Penalize invalid card selections
             if "Invalid number of cards" in str(e):
-                self.last_error_penalty = -1.0
+                self.last_error_penalty = -10.0
 
             # Penalize any error a little bit
-            self.last_error_penalty += -1.0
+            self.last_error_penalty += -10.0
 
             # Recovery: just refresh game state
             resp = self.client.send_message("get_game_state", {})
